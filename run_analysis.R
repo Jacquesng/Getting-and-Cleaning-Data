3dd95ab3696cb312bf54de04f3de488d2c9e8de9
Jacques Nack <jacquesng@gmail.com>
2017-03-26 23:43:59 -0700

    - Changed ./data” folder to ./getdata

--- run_analysis.R
+++ run_analysis.R
@@ -1,28 +1,28 @@
 ## 1 - Downloading and unzipping dataset
 
-if(!file.exists("./data")){dir.create("./data")}
+if(!file.exists("./getdata")){dir.create("./getdata")}
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(fileUrl,destfile="./data/Dataset.zip")
+download.file(fileUrl,destfile="./getdata/Dataset.zip")
 
 # Unzip dataSet to /data directory
-unzip(zipfile="./data/Dataset.zip",exdir="./data")
+unzip(zipfile="./getdata/Dataset.zip",exdir="./getdata")
 
 ## 2 - Merging the training set and the test sets to create one dataset
 # Reading trainings tables:
-x_train <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
-y_train <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
-subject_train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
+x_train <- read.table("./getdata/UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("./getdata/UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("./getdata/UCI HAR Dataset/train/subject_train.txt")
 
 # Reading testing tables:
-x_test <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
-y_test <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
-subject_test <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
+x_test <- read.table("./getdata/UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("./getdata/UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("./getdata/UCI HAR Dataset/test/subject_test.txt")
 
 # Reading feature vector:
-features <- read.table('./data/UCI HAR Dataset/features.txt')
+features <- read.table('./getdata/UCI HAR Dataset/features.txt')
 
 # Reading activity labels:
-activityLabels = read.table('./data/UCI HAR Dataset/activity_labels.txt')
+activityLabels = read.table('./getdata/UCI HAR Dataset/activity_labels.txt')
 
 colnames(x_train) <- features[,2] 
 colnames(y_train) <-"activityId"
